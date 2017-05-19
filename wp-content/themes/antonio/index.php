a67569489077ab773754d4c4e941ba3ab70cf00d
studentetc <studente.pierro@gmail.com>
2017-05-19 12:47:19 +0200

    Add files via upload

--- wp-content/themes/antonio/index.php
+++ wp-content/themes/antonio/index.php
@@ -1,35 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title></title>
-  <link rel="stylesheet" type="text/css" href="vendors/css/bootstrap.min.css">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css" href="css/main.css">
-
-</head>
-<body>
-  <div class="jumbotron">
-    <div class="container">
-      <nav class="navbar navbar-default">
-        <div class="container-fluid">
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#"></a>
-          </div>
-          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav navbar-right">
-              <li><a href="#">Digital</a></li>
-              <li><a href="#">Web</a></li>
-              <li><a href="#contact">Contact us</a></li>
-            </ul>
-          </div><!-- /.navbar-collapse -->
-        </div><!-- /.container-fluid -->
-      </nav>
+<?php get_header(); ?>
       <div class="row">
         <div class="col-md-12 welcome">
           <h1>Welcome to the course!</h1>
@@ -72,9 +41,4 @@
   </div>
 </div>
 </div>
-<script src="vendors/js/jquery.min.js"></script>
-<script src="vendors/js/bootstrap.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-validator/0.5.3/js/bootstrapValidator.min.js"></script>
-<script src="js/main.js"></script>
-</body>
-</html>
+<?php get_footer(); ?>
