c729b0c252b396370a9d5b2cabd9bcf9eb634940
studentetc <studente.pierro@gmail.com>
2017-05-19 12:14:36 +0200

    Add files via upload

---
+++ wp-content/themes/antonio/footer.php
@@ -0,0 +1,6 @@
+<script src="vendors/js/jquery.min.js"></script>
+<script src="vendors/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-validator/0.5.3/js/bootstrapValidator.min.js"></script>
+<script src="js/main.js"></script>
+</body>
+</html>
