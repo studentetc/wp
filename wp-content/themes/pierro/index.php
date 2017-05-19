39d45e9973fe685646d7b617b52ec02a6a2bcee0
studentetc <studente.pierro@gmail.com>
2017-05-19 13:03:37 +0200

    Add files via upload

--- wp-content/themes/pierro/index.php
+++ wp-content/themes/pierro/index.php
@@ -1,58 +1,44 @@
 <?php get_header(); ?>
-<div class="row">
-    <div class="col-md-12 welcome">
-        <h1><?php bloginfo(‘name’) ?></h1>
-        <h4><?php bloginfo(description) ?></h4>
-    </div>
-</div>      </div>
-    </div>
-    <div class="row" id="contact">
-    <div class="col-lg-12 text-center">
-        <h1>Contact us</h1>
-        <h3>Lorem ipsum dolor sit amet consectetur.</h3>
-    </div>
-</div>
-<form>
-    <div class="row row-form">
-        <div class="col-lg-12">
-            <div class="col-lg-6">
-                <div class="form-group">
-                    <label>Name</label>
-                    <div class="input-group">
-                        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                        <input name="name" type="text" class="form-control input-lg" placeholder="Your name" required>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label>Email address</label>
-                        <div class="input-group">
-        <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
-                    <input name="email" type="email" class="form-control input-lg" placeholder="Email" required>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label>Phone</label>
-                    <div class="input-group">
-                        <span class="input-group-addon"><i class="glyphicon glyphicon-earphone"></i></span>
-                        <input name="phone" type="number" class="form-control input-lg" placeholder="Your Phone Number" required>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-6">
-                <div class="form-group">
-                    <label>Message</label>
-                    <textarea spellcheck class="form-control input-lg" placeholder="Please, write a message"></textarea>
-                </div>
-            </div>
+      <div class="row">
+        <div class="col-md-12 welcome">
+          <h1><?php blog_info(name) ?></h1>
+          <h4><?php blog_info(description) ?></h4>
         </div>
-        <div class="row">
-            <div class="col-lg-12 text-center">
-                <button type="submit" class="btn btn-primary btn-lg">Submit</button>
-            </div>
-        </div>
+      </div>
     </div>
-</form>
+  </div>
+  <div class="row text-center" id="contact">
+    <h1>Contact us</h1>
+    <h2>Lorem ipsum dolor sit amet</h2>
+  </div>
+  <div class="row form-row">
+    <form>
+      <div class="col-md-6">
+        <div class="form-group">
+          <label for="exampleInputEmail1">Name</label>
+          <input type="text" class="form-control input-lg" id="exampleInputEmail1" placeholder="Insert your name" name="name" required>
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Email</label>
+          <input type="email" class="form-control input-lg" id="exampleInputPassword1" placeholder="Insert your email" name="email" required>
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Phone number</label>
+          <input type="number" class="form-control input-lg" id="exampleInputPassword1" placeholder="Insert your phone number" required>
+        </div>
+
+      </div>
+      <div class="col-md-6">
+        <label for="exampleInputPassword1">Insert a comment</label>
+        <textarea class="form-control input-lg"></textarea>
+      </div>
+      <div class="col-md-12">
+        <div class="text-center btn-container">
+          <button type="button" class="btn btn-primary btn-lg">Submit</button>
+        </div>
+      </div>
+    </form>
+  </div>
 </div>
 </div>
 <?php get_footer(); ?>
-
