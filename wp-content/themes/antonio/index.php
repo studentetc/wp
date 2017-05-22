93a2e92d3df5442449343f372894f3bdfdbda65e
studentetc <studente.pierro@gmail.com>
2017-05-22 12:17:27 +0200

    Add files via upload

--- wp-content/themes/antonio/index.php
+++ wp-content/themes/antonio/index.php
@@ -8,8 +8,26 @@
     </div>
   </div>
   <div class="row text-center" id="contact">
-    <h1>Contact us</h1>
-    <h2>Lorem ipsum dolor sit amet</h2>
+
+<?php
+                $args = array(
+                    'page_id' => '8' // the id of your page
+                );
+                $wp_query = new WP_Query($args);
+                if($wp_query->have_posts()) :
+                    while ($wp_query->have_posts()) :
+                        $wp_query->the_post();
+                      ?>
+                      <h1><?php the_title(); ?></h1>
+                      <h2><?php the_content(); ?></h2>
+                      <?php
+                    endwhile;
+                endif;
+ ?>
+
+    
+    
+
   </div>
   <div class="row form-row">
     <form>
