16c0364ef9315f61f506bf6dc63c13f6f606c5d7
studentetc <studente.pierro@gmail.com>
2017-05-22 11:51:39 +0200

    Add files via upload

--- wp-content/themes/antonio/index.php
+++ wp-content/themes/antonio/index.php
@@ -13,30 +13,7 @@
   </div>
   <div class="row form-row">
     <form>
-      <div class="col-md-6">
-        <div class="form-group">
-          <label for="exampleInputEmail1">Name</label>
-          <input type="text" class="form-control input-lg" id="exampleInputEmail1" placeholder="Insert your name" name="name" required>
-        </div>
-        <div class="form-group">
-          <label for="exampleInputPassword1">Email</label>
-          <input type="email" class="form-control input-lg" id="exampleInputPassword1" placeholder="Insert your email" name="email" required>
-        </div>
-        <div class="form-group">
-          <label for="exampleInputPassword1">Phone number</label>
-          <input type="number" class="form-control input-lg" id="exampleInputPassword1" placeholder="Insert your phone number" required>
-        </div>
-
-      </div>
-      <div class="col-md-6">
-        <label for="exampleInputPassword1">Insert a comment</label>
-        <textarea class="form-control input-lg"></textarea>
-      </div>
-      <div class="col-md-12">
-        <div class="text-center btn-container">
-          <button type="button" class="btn btn-primary btn-lg">Submit</button>
-        </div>
-      </div>
+      <?php echo do_shortcode('[contact-form-7 id="4" title="Contact form 1"]'); ?>
     </form>
   </div>
 </div>
