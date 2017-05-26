a7ed80c3d20f839ed3213c08bb4dbe5f92828c27
studentetc <studente.pierro@gmail.com>
2017-05-26 16:21:52 +0200

    Add files via upload

---
+++ wp-content/plugins/conductor-query-builder/includes/admin/class-conductor-query-builder-admin-views.php
@@ -0,0 +1,207 @@
+<?php
+/**
+ * Conductor Admin Query Builder Views (controller)
+ *
+ * @class Conductor_Query_Builder_Admin_Views
+ * @author Slocum Studio
+ * @version 1.0.3
+ * @since 1.0.0
+ */
+
+// Bail if accessed directly
+if ( ! defined( 'ABSPATH' ) )
+	exit;
+
+if ( ! class_exists( 'Conductor_Query_Builder_Admin_Views' ) ) {
+	final class Conductor_Query_Builder_Admin_Views {
+		/**
+		 * @var string
+		 */
+		public $version = '1.0.3';
+
+		/**
+		 * @var array
+		 */
+		public static $options = false;
+		/**
+		 * @var Conductor, Instance of the class
+		 */
+		protected static $_instance;
+
+		/**
+		 * Function used to create instance of class.
+		 */
+		public static function instance() {
+			if ( is_null( self::$_instance ) )
+				self::$_instance = new self();
+
+			return self::$_instance;
+		}
+
+
+		/**
+		 * This function sets up all of the actions and filters on instance. It also loads (includes)
+		 * the required files and assets.
+		 */
+		function __construct() {
+			add_action( 'admin_footer', array( $this, 'admin_footer' ) ); // Admin Footer
+		}
+
+		/**
+		 * This function outputs scripts in the admin footer.
+		 */
+		public function admin_footer( $the_hook_suffix = '' ) {
+			global $post, $hook_suffix;
+
+			// If we don't have a hook suffix argument
+			$the_hook_suffix = ( $the_hook_suffix === '' ) ? $hook_suffix : $the_hook_suffix;
+
+			// Grab the Conductor Query Builder instance
+			$conductor_query_builder = Conduct_Query_Builder();
+
+			// Grab the post meta
+			$post_meta = $conductor_query_builder->get_post_meta( get_post_field( 'ID', $post ) );
+
+			// Grab the Conductor Widget instance
+			$conductor_widget = Conduct_Widget();
+
+			// Grab the Conductor Widget instance data
+			$conductor_widget_instance = $conductor_query_builder->get_conductor_widget_instance( get_post_field( 'ID', $post ) );
+
+			// Meta Box Query Builder Tab Content Actions UnderscoreJS Template
+			self::meta_box_query_builder_tab_content_actions( $post, $post_meta, $conductor_query_builder, $conductor_widget_instance, $conductor_widget );
+
+			// Meta Box Query Builder Tab Content Clause Group UnderscoreJS Template
+			self::meta_box_query_builder_tab_content_clause_group( $post, $post_meta, $conductor_query_builder, $conductor_widget_instance, $conductor_widget );
+
+			// Meta Box Query Builder Tab Content Sub-Clause Group UnderscoreJS Template
+			self::meta_box_query_builder_tab_content_sub_clause_group( $post, $post_meta, $conductor_query_builder, $conductor_widget_instance, $conductor_widget );
+
+			// If we're on a page that supports the Conductor Query Builder
+			if ( apply_filters( 'conductor_query_builder_admin_footer', $the_hook_suffix && in_array( $the_hook_suffix, array( 'post.php', 'post-new.php', 'page.php', 'page-new.php' ) ), $the_hook_suffix, $hook_suffix, $post, $conductor_query_builder, $conductor_widget, $this ) ) {
+				// Shortcode query builder template
+				$conductor_query_builder->shortcode_query_builder();
+
+				// Shortcode insert UnderscoreJS Template
+				self::shortcode_query_builder_insert( $post, $post_meta,$conductor_query_builder, $conductor_widget_instance, $conductor_widget );
+
+				// Shortcode actions UnderscoreJS Template
+				self::shortcode_query_builder_actions_js( $post, $post_meta,$conductor_query_builder, $conductor_widget_instance, $conductor_widget );
+
+				// Shortcode create title UnderscoreJS Template
+				self::shortcode_query_builder_create_title_js( $post, $post_meta,$conductor_query_builder, $conductor_widget_instance, $conductor_widget );
+			}
+		}
+
+
+		/********************
+		 * Helper Functions *
+		 ********************/
+
+		// TODO: Filters here to allow for templates to be over-ridden
+
+		/**
+		 * This function renders the shortcode insert query builder content.
+		 */
+		public static function shortcode_query_builder_insert_tab_content( $post, $post_meta, $conductor_query_builder, $conductor_widget_instance, $conductor_widget ) {
+			require 'views/html-shortcode-query-builder-insert-tab-content.php';
+		}
+
+		/**
+		 * This function renders the shortcode create query builder content.
+		 */
+		public static function shortcode_query_builder_create_tab_content( $post, $post_meta, $conductor_query_builder, $conductor_widget_instance, $conductor_widget, $form_element ) {
+			require 'views/html-shortcode-query-builder-create-tab-content.php';
+		}
+
+		/**
+		 * This function renders the shortcode actions query builder content.
+		 */
+		public static function shortcode_query_builder_actions( $post, $post_meta, $conductor_query_builder, $conductor_widget_instance, $conductor_widget ) {
+			require 'views/html-shortcode-query-builder-actions.php';
+		}
+
+		/**
+		 * This function renders the shortcode query builder insert UnderscoreJS template.
+		 */
+		public static function shortcode_query_builder_insert( $post, $post_meta, $conductor_query_builder, $conductor_widget_instance, $conductor_widget ) {
+			require_once 'views/js-shortcode-query-builder-insert.php';
+		}
+
+		/**
+		 * This function renders the shortcode query builder actions UnderscoreJS template.
+		 */
+		public static function shortcode_query_builder_actions_js( $post, $post_meta, $conductor_query_builder, $conductor_widget_instance, $conductor_widget ) {
+			require_once 'views/js-shortcode-query-builder-actions.php';
+		}
+
+		/**
+		 * This function renders the shortcode query builder create title UnderscoreJS template.
+		 */
+		public static function shortcode_query_builder_create_title_js( $post, $post_meta, $conductor_query_builder, $conductor_widget_instance, $conductor_widget ) {
+			require_once 'views/js-shortcode-query-builder-create-title.php';
+		}
+
+		/**
+		 * This function renders the meta box query builder tab content.
+		 */
+		public static function meta_box_query_builder_tab_content( $post, $post_meta, $conductor_query_builder, $conductor_widget_instance, $conductor_widget ) {
+			require 'views/html-meta-box-query-builder-tab-content.php';
+		}
+
+		/**
+		 * This function renders the meta box query builder tab content actions UnderscoreJS template.
+		 */
+		public static function meta_box_query_builder_tab_content_actions( $post, $post_meta, $conductor_query_builder, $conductor_widget_instance, $conductor_widget ) {
+			require_once 'views/js-meta-box-query-builder-tab-content-actions.php';
+		}
+
+		/**
+		 * This function renders the meta box query builder tab content clause group UnderscoreJS template.
+		 */
+		public static function meta_box_query_builder_tab_content_clause_group( $post, $post_meta, $conductor_query_builder, $conductor_widget_instance, $conductor_widget ) {
+			require_once 'views/js-meta-box-query-builder-tab-content-clause-group.php';
+		}
+
+		/**
+		 * This function renders the meta box query builder tab content sub-clause group UnderscoreJS template.
+		 */
+		public static function meta_box_query_builder_tab_content_sub_clause_group( $post, $post_meta, $conductor_query_builder, $conductor_widget_instance, $conductor_widget ) {
+			// TODO: Adjust filter name
+			$field_brackets = apply_filters( 'conductor_query_builder_sub_clause_group_field_brackets', array( 'left' => '[', 'right' => ']' ), $post, $post_meta, $conductor_query_builder, $conductor_widget_instance, $conductor_widget );
+
+			// Setup the left and right field brackets
+			$left_field_bracket = esc_attr( $field_brackets['left'] );
+			$right_field_bracket = esc_attr( $field_brackets['right'] );
+
+			// Setup the meta key prefix
+			// TODO: Adjust filter name
+			$meta_key_prefix = apply_filters( 'conductor_query_builder_sub_clause_group_meta_key_prefix', esc_attr( rtrim( $conductor_query_builder->meta_key_prefix, '_' ) ), $conductor_query_builder->meta_key_prefix, $field_brackets, $left_field_bracket, $right_field_bracket, $post, $post_meta, $conductor_query_builder, $conductor_widget_instance, $conductor_widget );
+
+			require_once 'views/js-meta-box-query-builder-tab-content-sub-clause-group.php';
+		}
+
+		/**
+		 * This function renders the meta box output tab content.
+		 */
+		public static function meta_box_output_tab_content( $post, $post_meta, $conductor_query_builder, $conductor_widget_instance, $conductor_widget ) {
+			require 'views/html-meta-box-output-tab-content.php';
+		}
+
+		/**
+		 * This function renders the meta box advanced tab content.
+		 */
+		public static function meta_box_advanced_tab_content( $post, $post_meta, $conductor_query_builder, $conductor_widget_instance, $conductor_widget ) {
+			require 'views/html-meta-box-advanced-tab-content.php';
+		}
+	}
+
+	/**
+	 * Create an instance of the Conductor_Query_Builder_Admin_Views class.
+	 */
+	function Conduct_Query_Builder_Admin_Views() {
+		return Conductor_Query_Builder_Admin_Views::instance();
+	}
+
+	Conduct_Query_Builder_Admin_Views(); // Conduct your content!
+}
---
+++ wp-content/plugins/conductor-query-builder/includes/admin/views/js-meta-box-query-builder-tab-content-sub-clause-group.php
@@ -0,0 +1,157 @@
+<?php
+/**
+ * Meta Box Query Builder Tab Content Clause Sub-Group UnderscoreJS Template
+ *
+ * @var $post WP_Post
+ * @var $post_meta array
+ * @var $conductor_query_builder Conductor_Query_Builder
+ * @var $conductor_widget_instance array
+ * @var $conductor_widget Conductor_Widget
+ * @var $field_brackets array
+ * @var $left_field_bracket string
+ * @var $right_field_bracket string
+ * @var $meta_key_prefix string
+ */
+?>
+
+<script type="text/template" id="tmpl-conductor-qb-meta-box-query-builder-sub-clause-group">
+	<?php do_action( 'conductor_query_builder_meta_box_query_builder_sub_clause_group_before', $post, $post_meta, $conductor_widget_instance, $conductor_widget, $conductor_query_builder ); ?>
+
+	<?php do_action( 'conductor_query_builder_meta_box_query_builder_sub_clause_group_label_before', $post, $post_meta, $conductor_widget_instance, $conductor_widget, $conductor_query_builder ); ?>
+
+	<span class="descriptive-label conductor-qb-descriptive-label descriptive-label-and conductor-qb-descriptive-label-and">{{{ conductor_query_builder.l10n.and }}}</span>
+
+	<?php do_action( 'conductor_query_builder_meta_box_query_builder_sub_clause_group_label_after', $post, $post_meta, $conductor_widget_instance, $conductor_widget, $conductor_query_builder ); ?>
+
+	<#
+		/*
+		 * Parameters Column (1st Column)
+		 */
+
+		// If we should render this column
+		if ( data.columns.parameters || data.meta.parameters ) {
+	#>
+			<select class="conductor-qb-select conductor-qb-select2 conductor-qb-parameters-select conductor-qb-{{ data.type }}-select conductor-qb-{{ data.type }}-parameters-select" name="<?php echo $meta_key_prefix; ?><?php echo $left_field_bracket; ?>{{ data.type }}<?php echo $right_field_bracket; ?><?php echo $left_field_bracket; ?>{{ data.parent.count }}<?php echo $right_field_bracket; ?><?php echo $left_field_bracket; ?>{{ data.count }}<?php echo $right_field_bracket; ?><?php echo $left_field_bracket; ?>parameters<?php echo $right_field_bracket; ?><# if ( data.columns.parameters.select2 && data.columns.parameters.select2.multiple ) { #>[]<# } #>"<# if ( data.columns.parameters.select2 && data.columns.parameters.select2.multiple ) { #> multiple="multiple" <# } #> <# if ( data.columns.parameters.select2 && data.columns.parameters.select2.placeholder ) { #> data-placeholder="{{ data.columns.parameters.select2.placeholder }}" <# } #> <# if ( data.columns.parameters.select2 && data.columns.parameters.select2.tags ) { #> data-tags="true" <# } #> <# if ( data.columns.parameters.select2 && data.columns.parameters.select2['toggle-action-buttons'] ) { #> data-toggle-action-buttons="true" <# } #> data-select-type="{{ data.type }}" data-type="parameters">
+				<option value=""></option>
+				<#
+					// Loop through the parameters
+					_.each( data.parameters, function ( parameter_data, parameter ) {
+				#>
+						<option value="{{ parameter }}" <# if ( data.meta.parameters && ( ( typeof data.meta.parameters === 'string' && parameter === data.meta.parameters ) || ( typeof data.meta.parameters !== 'string' && data.meta.parameters.length && data.meta.parameters.indexOf( parameter ) !== -1 ) ) ) { #> <?php selected( true ); ?> <# } #> <# if ( typeof parameter_data !== 'string' && parameter_data.parameter ) { #> data-parameter="{{ parameter_data.parameter }}" <# } #> <# if ( typeof parameter_data !== 'string' && parameter_data.field ) { #> data-field="{{ parameter_data.field }}" <# } #>>
+							<#
+								if ( typeof parameter_data === 'string' ) {
+							#>
+									{{{ parameter_data }}}
+							<#
+								}
+								else {
+							#>
+									{{{ parameter_data.label }}}
+							<#
+								}
+							#>
+						</option>
+				<#
+					} );
+				#>
+			</select>
+	<#
+		}
+	#>
+
+
+	<#
+		/*
+		 * Operators Column (2nd Column)
+		 */
+
+		// If we should render this column
+		if ( data.columns.operators || data.meta.operators ) {
+	#>
+			<select class="conductor-qb-select conductor-qb-select2 conductor-qb-operators-select conductor-qb-{{ data.type }}-select conductor-qb-{{ data.type }}-operators-select" name="<?php echo $meta_key_prefix; ?><?php echo $left_field_bracket; ?>{{ data.type }}<?php echo $right_field_bracket; ?><?php echo $left_field_bracket; ?>{{ data.parent.count }}<?php echo $right_field_bracket; ?><?php echo $left_field_bracket; ?>{{ data.count }}<?php echo $right_field_bracket; ?><?php echo $left_field_bracket; ?>operators<?php echo $right_field_bracket; ?><# if ( data.columns.operators.select2 && data.columns.operators.select2.multiple ) { #>[]<# } #>" <# if ( data.columns.operators.select2 && data.columns.operators.select2.multiple ) { #> multiple="multiple" <# } #> <# if ( data.columns.operators.select2 && data.columns.operators.select2.placeholder ) { #> data-placeholder="{{ data.columns.operators.select2.placeholder }}" <# } #> <# if ( data.columns.parameters.select2 && data.columns.parameters.select2.tags ) { #> data-tags="true" <# } #> data-select-type="{{ data.type }}" data-type="operators" <# if ( ! data.meta.operators ) {#> disabled="disabled" <# } #>>
+				<option value=""></option>
+				<#
+					// Loop through the operators
+					_.each( data.operators, function ( operator_data, operator ) {
+				#>
+						<option value="{{ operator }}" <# if ( data.meta.operators && ( ( typeof data.meta.operators === 'string' && operator === data.meta.operators ) || ( data.meta.operators.length && data.meta.operators.indexOf( operator ) !== -1 ) ) ) { #> <?php selected( true ); ?> <# } #> <# if ( ( typeof operator_data !== 'string' ) && operator_data.multiple ) { #> data-multiple="true" <# } #>>
+							<#
+								if ( typeof operator_data === 'string' ) {
+							#>
+									{{{ operator_data }}}
+							<#
+								}
+								else {
+							#>
+									{{{ operator_data.label }}}
+							<#
+								}
+							#>
+						</option>
+				<#
+					} );
+				#>
+			</select>
+	<#
+		}
+	#>
+
+
+	<#
+		/*
+		 * Values Column (3rd Column)
+		 */
+
+		// If we should render this column
+		if ( data.columns.values ) {
+	#>
+			<select class="conductor-qb-select conductor-qb-select2 conductor-qb-values-select conductor-qb-{{ data.type }}-select conductor-qb-{{ data.type }}-values-select" name="<?php echo $meta_key_prefix; ?><?php echo $left_field_bracket; ?>{{ data.type }}<?php echo $right_field_bracket; ?><?php echo $left_field_bracket; ?>{{ data.parent.count }}<?php echo $right_field_bracket; ?><?php echo $left_field_bracket; ?>{{ data.count }}<?php echo $right_field_bracket; ?><?php echo $left_field_bracket; ?>values<?php echo $right_field_bracket; ?><# if ( data.columns.values.select2 && data.columns.values.select2.multiple ) { #>[]<# } #>"<# if ( data.columns.values.select2 && data.columns.values.select2.multiple ) { #> multiple="multiple" <# } #> <# if ( data.columns.values.select2 && data.columns.values.select2.placeholder ) { #> data-placeholder="{{ data.columns.values.select2.placeholder }}" <# } #> <# if ( data.columns.values.select2 && data.columns.values.select2.tags ) { #> data-tags="true" <# } #> <# if ( data.columns.values.select2 && data.columns.values.select2['toggle-action-buttons'] ) { #> data-toggle-action-buttons="true" <# } #> data-select-type="{{ data.type }}" data-type="values" <# if ( ! data.meta.values ) {#> disabled="disabled" <# } #> data-selected-options="{{ ( typeof data.meta.values === 'string' ) ? data.meta.values : JSON.stringify( ( data.meta.values && data.meta.values.length ) ? data.meta.values.map( String ) : [] ) }}">
+				<option value=""></option>
+				<#
+					// Loop through the values
+					_.each( data.values, function ( value_data, value ) {
+						// Grab the value
+						value = ( typeof value_data === 'string' ) ? value_data : value;
+				#>
+						<option value="{{ value }}" <# if ( data.meta.values && ( ( typeof data.meta.values === 'string' && value === data.meta.values ) || ( data.meta.values.length && data.meta.values.indexOf( value ) !== -1 ) ) ) { #> <?php selected( true ); ?> <# } #> <# if ( typeof value_data !== 'string' && value_data.type ) { #> data-type="{{ value_data.type }}" <# } #>>
+							<#
+								if ( typeof value_data === 'string' ) {
+							#>
+									{{{ value }}}
+							<#
+								}
+								else {
+							#>
+									{{{ value_data.label }}}
+							<#
+								}
+							#>
+						</option>
+				<#
+					} );
+				#>
+			</select>
+	<#
+		}
+	#>
+
+	<#
+		// If this isn't the default view
+		if ( ! data.flags.default ) {
+	#>
+		<button class="button button-secondary conductor-qb-button conductor-qb-action-button conductor-qb-remove-action-button conductor-qb-remove-sub-clause-group-button conductor-qb-remove-{{ data.type }}-sub-clause-group-button" data-action-button-id="conductor-qb-add-{{ data.type }}-clause-group-button" title=<?php esc_attr_e( 'Remove Sub-Clause Group', 'conductor-query-builder' ); ?>><span class="dashicons dashicons-minus"></span></button>
+	<#
+		}
+	#>
+
+	<#
+		// If this view allows for sub-clause groups
+		if ( data.flags.sub_clause_groups ) {
+	#>
+		<button class="button button-secondary conductor-qb-button conductor-qb-action-button conductor-qb-add-action-button conductor-qb-add-sub-clause-group-button conductor-qb-add-{{ data.type }}-sub-clause-group-button" title="<?php esc_attr_e( 'Add Sub-Clause Group', 'conductor-query-builder' ); ?>"><span class="dashicons dashicons-plus"></span></button>
+	<#
+		}
+	#>
+
+	<?php do_action( 'conductor_query_builder_meta_box_query_builder_sub_clause_group_after', $post, $post_meta, $conductor_widget_instance, $conductor_widget, $conductor_query_builder ); ?>
+</script>
